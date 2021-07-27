11160feb93b1766c939d977b6423e05aae85f2d5
Lion1Blue <58215811+Lion1Blue@users.noreply.github.com>
2021-07-27 10:20:28 +0200

    Add files via upload

---
+++ Visual/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Visual
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plotView1 = new OxyPlot.WindowsForms.PlotView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxXMin = new System.Windows.Forms.TextBox();
+            this.textBoxXMax = new System.Windows.Forms.TextBox();
+            this.labelXMin = new System.Windows.Forms.Label();
+            this.labelXMax = new System.Windows.Forms.Label();
+            this.labelLagrange = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // plotView1
+            // 
+            this.plotView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.plotView1.Location = new System.Drawing.Point(13, 9);
+            this.plotView1.Name = "plotView1";
+            this.plotView1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView1.Size = new System.Drawing.Size(524, 429);
+            this.plotView1.TabIndex = 0;
+            this.plotView1.Text = "plotView1";
+            this.plotView1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(713, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(543, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(245, 258);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // textBoxXMin
+            // 
+            this.textBoxXMin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxXMin.Location = new System.Drawing.Point(581, 298);
+            this.textBoxXMin.Name = "textBoxXMin";
+            this.textBoxXMin.Size = new System.Drawing.Size(100, 20);
+            this.textBoxXMin.TabIndex = 3;
+            this.textBoxXMin.Text = "-100";
+            this.textBoxXMin.TextChanged += new System.EventHandler(this.textBoxXMin_TextChanged);
+            this.textBoxXMin.Validating += new System.ComponentModel.CancelEventHandler(this.textBoxXMin_Validating);
+            // 
+            // textBoxXMax
+            // 
+            this.textBoxXMax.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxXMax.Location = new System.Drawing.Point(581, 324);
+            this.textBoxXMax.Name = "textBoxXMax";
+            this.textBoxXMax.Size = new System.Drawing.Size(100, 20);
+            this.textBoxXMax.TabIndex = 4;
+            this.textBoxXMax.Text = "100";
+            this.textBoxXMax.TextChanged += new System.EventHandler(this.textBoxXMax_TextChanged);
+            this.textBoxXMax.Validating += new System.ComponentModel.CancelEventHandler(this.textBoxXMax_Validating);
+            // 
+            // labelXMin
+            // 
+            this.labelXMin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelXMin.AutoSize = true;
+            this.labelXMin.Location = new System.Drawing.Point(540, 301);
+            this.labelXMin.Name = "labelXMin";
+            this.labelXMin.Size = new System.Drawing.Size(31, 13);
+            this.labelXMin.TabIndex = 5;
+            this.labelXMin.Text = "XMin";
+            // 
+            // labelXMax
+            // 
+            this.labelXMax.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelXMax.AutoSize = true;
+            this.labelXMax.Location = new System.Drawing.Point(540, 327);
+            this.labelXMax.Name = "labelXMax";
+            this.labelXMax.Size = new System.Drawing.Size(34, 13);
+            this.labelXMax.TabIndex = 6;
+            this.labelXMax.Text = "XMax";
+            // 
+            // labelLagrange
+            // 
+            this.labelLagrange.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelLagrange.AutoSize = true;
+            this.labelLagrange.Location = new System.Drawing.Point(544, 10);
+            this.labelLagrange.Name = "labelLagrange";
+            this.labelLagrange.Size = new System.Drawing.Size(82, 13);
+            this.labelLagrange.TabIndex = 7;
+            this.labelLagrange.Text = "Lagrange-Table";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelLagrange);
+            this.Controls.Add(this.labelXMax);
+            this.Controls.Add(this.labelXMin);
+            this.Controls.Add(this.textBoxXMax);
+            this.Controls.Add(this.textBoxXMin);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.plotView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OxyPlot.WindowsForms.PlotView plotView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxXMin;
+        private System.Windows.Forms.TextBox textBoxXMax;
+        private System.Windows.Forms.Label labelXMin;
+        private System.Windows.Forms.Label labelXMax;
+        private System.Windows.Forms.Label labelLagrange;
+    }
+}
+
---
+++ Visual/Form1.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OxyPlot;
+using OxyPlot.Series;
+using Polynominterpolation;
+
+namespace Visual
+{
+    public partial class Form1 : Form
+    {
+        PlotModel plotModel;
+        bool onMouseHold = false;
+        int indexOfCurrentSelectedPoint = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Visualisation Polynomial Functions";
+            dataGridView1.RowCount++;
+            dataGridView1.ColumnCount = 2;
+            dataGridView1.Columns[0].HeaderText = "x";
+            dataGridView1.Columns[1].HeaderText = "f(x)";
+
+            plotModel = new PlotModel();
+            plotModel.Series.Add(new LineSeries());
+            plotModel.Series.Add(new LineSeries());
+
+            //Adding y - Coordinate Axes
+            plotModel.Axes.Add(new OxyPlot.Axes.LinearAxis
+            { 
+                Position = OxyPlot.Axes.AxisPosition.Bottom,
+                ExtraGridlines = new double[] { 0 },
+                ExtraGridlineThickness = 1,
+                ExtraGridlineColor = OxyColors.Black,
+                MajorGridlineThickness = 1,
+                MinorGridlineStyle = LineStyle.Solid,
+                MajorGridlineStyle = OxyPlot.LineStyle.Solid }
+            );
+
+            //Adding x - Coordinate Axes
+            plotModel.Axes.Add(new OxyPlot.Axes.LinearAxis
+            { 
+                Position = OxyPlot.Axes.AxisPosition.Left, 
+                ExtraGridlines = new double[] { 0 }, 
+                ExtraGridlineThickness = 1, 
+                ExtraGridlineColor = OxyColors.Black, 
+                MajorGridlineThickness = 1, 
+                MinorGridlineStyle = LineStyle.Solid, 
+                MajorGridlineStyle = OxyPlot.LineStyle.Solid }
+            );
+
+
+            plotView1.MouseUp += PlotView_MouseUp;
+
+            InitalizeMouseEvents(plotModel);
+
+            plotView1.Model = plotModel;
+            plotModel.InvalidatePlot(true);
+        }
+
+        private void PlotView_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && onMouseHold)
+            {
+                onMouseHold = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DrawAll();
+        }
+
+        private void InitalizeMouseEvents(PlotModel myModel)
+        {
+            PointD[] points = GetPointsFromGridPointD(dataGridView1);
+            LineSeries controlPoints = new LineSeries();
+            controlPoints.MarkerType = MarkerType.Circle;
+            controlPoints.Color = OxyColors.Red;
+            controlPoints.LineStyle = LineStyle.None;
+
+            myModel.MouseDown += (s, e) =>
+            {
+                double x, y;
+
+                PointD[] pointsLocal = GetPointsFromGridPointD(dataGridView1);
+
+                x = controlPoints.InverseTransform(e.Position).X;
+                y = controlPoints.InverseTransform(e.Position).Y;
+
+                //Get the the distance from two points 9 pixels apart
+                ScreenPoint screenPointOne = new ScreenPoint(0, 0);
+                ScreenPoint screenPointTwo = new ScreenPoint(5, 5);
+
+                DataPoint dataPointOne = controlPoints.InverseTransform(screenPointOne);
+                DataPoint dataPointTwo = controlPoints.InverseTransform(screenPointTwo);
+
+                double dx = dataPointOne.X - dataPointTwo.X;
+                double dy = dataPointOne.Y - dataPointTwo.Y;
+
+                double distance = Math.Sqrt(dx * dx + dy * dy);
+
+                int index;
+
+                bool sucessfullGrap = IsInDistance(distance, pointsLocal, new PointD(x, y), out index);
+
+                //deletes Point
+                if (e.ChangedButton == OxyMouseButton.Right && sucessfullGrap)
+                {
+                    dataGridView1.Rows.RemoveAt(index);
+                    pointsLocal = GetPointsFromGridPointD(dataGridView1);
+                    DrawFunction(myModel, pointsLocal);
+                    DrawControlPoints(myModel, controlPoints, pointsLocal);
+                    //Updates the View
+                    myModel.InvalidatePlot(true);
+
+                    return;
+                }
+
+                //adds new Point
+                if (e.ChangedButton == OxyMouseButton.Left && !sucessfullGrap)
+                {
+                    AppendPointToDataGridView(dataGridView1, new PointD(x, y));
+                    pointsLocal = GetPointsFromGridPointD(dataGridView1);
+                    DrawFunction(myModel, pointsLocal);
+                    DrawControlPoints(myModel, controlPoints, pointsLocal);
+                    //Updates the View
+                    myModel.InvalidatePlot(true);
+
+                    return;
+                }
+
+                if (e.ChangedButton == OxyMouseButton.Left && sucessfullGrap)
+                {
+                    onMouseHold = true;
+                    indexOfCurrentSelectedPoint = index;
+                }
+
+            };
+
+            myModel.MouseMove += (s, e) =>
+            {
+                if (onMouseHold)
+                {
+                    PointD[] pointsLocal = GetPointsFromGridPointD(dataGridView1);
+
+                    double x, y;
+
+                    x = controlPoints.InverseTransform(e.Position).X;
+                    y = controlPoints.InverseTransform(e.Position).Y;
+
+                    dataGridView1.Rows[indexOfCurrentSelectedPoint].Cells[0].Value = x.ToString();
+                    dataGridView1.Rows[indexOfCurrentSelectedPoint].Cells[1].Value = y.ToString();
+
+                    DrawFunction(myModel, pointsLocal);
+                    DrawControlPoints(myModel, controlPoints, pointsLocal);
+                    myModel.InvalidatePlot(true);
+                }
+            };
+
+
+            DrawControlPoints(myModel, controlPoints, points);
+        }
+
+        private void DrawAll()
+        {
+            PointD[] points = GetPointsFromGridPointD(dataGridView1);
+            LineSeries controlPoints = new LineSeries();
+            controlPoints.MarkerType = MarkerType.Circle;
+            controlPoints.Color = OxyColors.Red;
+            controlPoints.LineStyle = LineStyle.None;
+
+            DrawFunction(plotModel, points);
+            DrawControlPoints(plotModel, controlPoints, points);
+
+            plotModel.InvalidatePlot(true);
+        }
+
+        private void DrawFunction(PlotModel myModel, PointD[] points)
+        {
+            LineSeries splineLine = new LineSeries();
+            splineLine.MarkerType = MarkerType.None;
+            splineLine.Color = OxyColors.Blue;
+
+            if (points.Length == 0)
+            {
+                myModel.Series[0] = splineLine;
+                return;
+            }
+
+            Term term = new Term(ConvertPointDToDoubleArray(points), points.Length - 1);
+
+            double xmin = Convert.ToDouble(textBoxXMin.Text);
+            double xmax = Convert.ToDouble(textBoxXMax.Text);
+
+            double stepSize = 0.1;
+
+            for (double x = xmin; x < xmax; x += stepSize)
+            {
+                splineLine.Points.Add(new DataPoint(x, term.calc(x)));
+            }
+
+            myModel.Series[0] = splineLine;
+        }
+
+        private void DrawControlPoints(PlotModel myModel, LineSeries controlPoints, PointD[] points)
+        {
+            controlPoints.Points.Clear();
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                controlPoints.Points.Add(new DataPoint(points[i].X, points[i].Y));
+            }
+
+            myModel.Series[1] = controlPoints;
+        }
+
+        public bool IsInDistance(double maxDistance, PointD[] points, PointD point, out int indexOfMin)
+        {
+            int currentIndex = 0;
+            double currentMaxDistance = maxDistance;
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                double dx = points[i].X - point.X;
+                double dy = points[i].Y - point.Y;
+
+                double distance = Math.Sqrt(dx * dx + dy * dy);
+                if (currentMaxDistance > distance)
+                {
+                    currentIndex = i;
+                    currentMaxDistance = distance;
+                }
+            }
+
+            if (maxDistance == currentMaxDistance)
+            {
+                indexOfMin = 0;
+                return false;
+            }
+
+
+            indexOfMin = currentIndex;
+            return true;
+        }
+
+        private PointD[] GetPointsFromGridPointD(DataGridView dataGridView)
+        {
+            PointD[] table = new PointD[dataGridView.RowCount - 1];
+
+            for (int i = 0; i < dataGridView.RowCount - 1; i++)
+            {
+                double x = Convert.ToDouble(dataGridView[0, i].Value.ToString());
+                double y = Convert.ToDouble(dataGridView[1, i].Value.ToString());
+
+                table[i] = new PointD(x, y);
+            }
+
+            return table;
+        }
+
+        private double[,] GetPointsFromGrid(DataGridView dataGridView)
+        {
+            double[,] table = new double[dataGridView.ColumnCount, dataGridView.RowCount - 1];
+
+            for (int i = 0; i < dataGridView.RowCount - 1; i++)
+            {
+                float x = Convert.ToSingle(dataGridView[0, i].Value.ToString());
+                float y = Convert.ToSingle(dataGridView[1, i].Value.ToString());
+
+                table[0, i] = x;
+                table[1, i] = y;
+            }
+
+            return table;
+        }
+
+        private void AppendPointToDataGridView(DataGridView dataGridView, PointD point)
+        {
+            dataGridView.RowCount++;
+            dataGridView.Rows[dataGridView.RowCount - 2].Cells[0].Value = point.X.ToString();
+            dataGridView.Rows[dataGridView.RowCount - 2].Cells[1].Value = point.Y.ToString();
+        }
+
+        private double[,] ConvertPointDToDoubleArray(PointD[] points)
+        {
+            double[,] table = new double[2, points.Length];
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                table[0, i] = points[i].X;
+                table[1, i] = points[i].Y;
+            }
+
+            return table;
+        }
+
+        private void textBoxXMin_TextChanged(object sender, EventArgs e)
+        {
+            if (!CheckTextBoxForDouble(((TextBox)sender)))
+                return;
+
+            DrawAll();
+        }
+
+        private void textBoxXMax_TextChanged(object sender, EventArgs e)
+        {
+            if (!CheckTextBoxForDouble(((TextBox)sender)))
+                return;
+
+            DrawAll();
+        }
+        private bool CheckTextBoxForDouble(TextBox textBox)
+        {
+            double number;
+            return double.TryParse(textBox.Text, out number);
+        }
+
+        private void textBoxXMin_Validating(object sender, CancelEventArgs e)
+        {
+            if (!CheckTextBoxForDouble(((TextBox)sender)))
+                e.Cancel = true;
+        }
+
+        private void textBoxXMax_Validating(object sender, CancelEventArgs e)
+        {
+            if (!CheckTextBoxForDouble(((TextBox)sender)))
+                e.Cancel = true;
+        }
+    }
+}
